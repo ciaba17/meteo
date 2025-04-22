fa4871bbd21cf2ef7fef12f6d08da4bd81005372
ciaba17 <gabrycdp@gmail.com>
2025-04-22 23:01:28 +0200

    primo commit dell'app effettiva

--- meteo/lib/main.dart
+++ meteo/lib/main.dart
@@ -1,122 +1,167 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(EcoMeteoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class EcoMeteoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white70,
+
+      // Appbar per logo e tasto menu
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      // Logo EcoMeteo
+        backgroundColor: Colors.transparent,
+        title: Row(
+          children: [
+            Icon(Icons.eco, color: Colors.green, size: 28),
+            SizedBox(width: 8),
+            Text("EcoMeteo",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green[800]))
+          ],
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+      // Menu laterale
+      endDrawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.green),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.eco, color: Colors.greenAccent, size: 28),
+                  Text("EcoMeteo",
+                    style: TextStyle(color: Colors.white, fontSize: 24)),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("Info"),
+              onTap: () => Navigator.pop(context),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Impostazioni"),
+              onTap: () => Navigator.pop(context),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Nome città e data a sinistra e simbolo meteo
+              Text("Roma",
+                  style:
+                      TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+              Text("24 aprile",
+                  style: TextStyle(fontSize: 20, color: Colors.grey[700])),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("20°",
+                      style:
+                          TextStyle(fontSize: 64, fontWeight: FontWeight.bold)),
+                  Icon(Icons.wb_cloudy, size: 72, color: Colors.grey[700])
+                ],
+              ),
+              // Caselle con le previsioni successive
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  forecastCard("Gio", Icons.cloud, "18°"),
+                  forecastCard("Ven", Icons.cloud, "20°"),
+                  forecastCard("Sab", Icons.wb_sunny, "22°"),
+                  forecastCard("Dom", Icons.wb_sunny_outlined, "21°"),
+                ],
+              ),
+              SizedBox(height: 50),
+              // giudizio ambientale
+              Text("GIUDIZIO",
+                  style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 48,
+                    backgroundColor: Colors.green,
+                    child: Icon(Icons.sentiment_satisfied_alt,
+                        size: 48, color: Colors.black),
+                  ),
+                  SizedBox(width: 24),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      infoBullet("> QUALITÀ ARIA"),
+                      infoBullet("> RISCALDAMENTO GLOBALE"),
+                      infoBullet("> ALTRO"),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 16),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                child: Text("75%",
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget forecastCard(String day, IconData icon, String temp) {
+    return Column(
+      children: [
+        Text(day,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        SizedBox(height: 4),
+        Icon(icon, size: 32, color: Colors.grey[700]),
+        SizedBox(height: 4),
+        Text(temp, style: TextStyle(fontSize: 16))
+      ],
+    );
+  }
+
+  Widget infoBullet(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6.0),
+      child: Text(text,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  void apriMenu() {
   }
 }
