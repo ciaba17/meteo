691281e9d36f798e5ba88ff4c8e9a04af1352778
ciaba17 <gabrycdp@gmail.com>
2025-04-24 14:45:38 +0200

    aggiunto bottone per dettagli meteo del giorno attuale

--- meteo/lib/main.dart
+++ meteo/lib/main.dart
@@ -34,14 +34,15 @@ class HomeScreen extends StatelessWidget {
             Icon(Icons.eco, color: Colors.green, size: 28),
             SizedBox(width: 8),
             Text("EcoWeather",
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green[800]))
-          ],
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.green[800]
+              )
+            )
+          ]
         ),
       ),
-
       // Menu laterale
       endDrawer: Drawer(
         child: ListView(
@@ -77,75 +78,79 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Nome città e data a sinistra e simbolo Weather
-              Text("Roma",
-                  style:
-                      TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-              Text("24 aprile",
-                  style: TextStyle(fontSize: 20, color: Colors.grey[700])),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("20°",
-                      style:
-                          TextStyle(fontSize: 64, fontWeight: FontWeight.bold)),
-                  Icon(Icons.wb_cloudy, size: 72, color: Colors.grey[700])
-                ],
-              ),
-              // Caselle con le previsioni successive
-              SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  forecastCard("Gio", Icons.cloud, "18°"),
-                  forecastCard("Ven", Icons.cloud, "20°"),
-                  forecastCard("Sab", Icons.wb_sunny, "22°"),
-                  forecastCard("Dom", Icons.wb_sunny_outlined, "21°"),
-                ],
-              ),
-              SizedBox(height: 50),
-
-              // giudizio ambientale
-              Center(
-                child: Column(
+              InkWell(
+                onTap: () => mostaDettagliMeteo(context),
+                // Nome città e data a sinistra e simbolo Weather
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("BILANCIO AMBIENTALE",
-                        style:
-                            TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                    Column(
+                      children: [
+                        Text("Roma",
+                          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                        Text("24 aprile",
+                          style: TextStyle(fontSize: 20, color: Colors.grey[700])),
+                        Text("20°",
+                          style:TextStyle(fontSize: 64, fontWeight: FontWeight.bold)),
+                      ],
                     ),
-
-                  SizedBox(height: 25),
-
-                    // Faccina
-                    CircleAvatar(
-                    radius: 48,
-                    backgroundColor: Colors.green,
-                    child: Icon(Icons.sentiment_satisfied_alt_outlined,
-                        size: 70, color: Colors.black),
-                  ),
-
-                  SizedBox(height: 20),
-
-                    // Bottone
-                    OutlinedButton(
-                      onPressed: () => mostraDettagli(context),
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: Colors.black, width: 2),
-                        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                    ),
-                      child : Text(
-                        "APPROFONDISCI",
-                        style: TextStyle(fontSize: 16),)
-                    ),
+                    Icon(Icons.wb_cloudy, size: 72, color: Colors.grey[700])
+                  ],
+                ),
+              ),
+                  // Caselle con le previsioni successive
+              SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    forecastCard("Gio", Icons.cloud, "18°"),
+                    forecastCard("Ven", Icons.cloud, "20°"),
+                    forecastCard("Sab", Icons.wb_sunny, "22°"),
+                    forecastCard("Dom", Icons.wb_sunny_outlined, "21°"),
                   ],
                 ),
 
+                SizedBox(height: 50),
+                
+                // giudizio ambientale
+                Center(
+                  child: Column(
+                    children: [
+                      Text("BILANCIO AMBIENTALE",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      
+                      SizedBox(height: 25),
+                
+                      // Faccina
+                      CircleAvatar(
+                      radius: 48,
+                      backgroundColor: Colors.green,
+                      child: Icon(Icons.sentiment_satisfied_alt_outlined,
+                          size: 70, color: Colors.black),
+                      ),
+                
+                        SizedBox(height: 20),
+                
+                        // Bottone
+                        OutlinedButton(
+                          onPressed: () => mostraInquinamento(context),
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Colors.black, width: 2),
+                            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          ),
+                          child : Text(
+                            "APPROFONDISCI",
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
           ),
         ),
-      ),
-    );
+      );
   }
 
   Widget forecastCard(String day, IconData icon, String temp) {
@@ -161,13 +166,13 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  void mostraDettagli(BuildContext context) {
+  void mostraInquinamento(BuildContext context) {
     Navigator.push(
       context,
       PageRouteBuilder(
         transitionDuration: Duration(milliseconds: 600),
         reverseTransitionDuration: Duration(milliseconds: 600),
-        pageBuilder: (context, animation, secondaryAnimation) => DettagliScreen(),
+        pageBuilder: (context, animation, secondaryAnimation) => InquinamentoScreen(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           final tween = Tween<Offset>(
             begin: Offset(0.0, 1.0), // da sotto verso il centro
@@ -182,11 +187,33 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+  
+  void mostaDettagliMeteo(BuildContext context) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration(milliseconds: 200),
+        reverseTransitionDuration: Duration(milliseconds: 100),
+        pageBuilder: (context, animation, secondaryAnimation) => DettagliMeteoScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          final scaleTween = Tween<double>(
+            begin: 0.7, // da sotto verso il centro
+            end:1.0,
+          ).chain(CurveTween(curve: Curves.easeInOut));
+
+          return ScaleTransition(
+            scale: animation.drive(scaleTween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
 }
 
 
-class DettagliScreen extends StatelessWidget {
-  const DettagliScreen({super.key});
+class InquinamentoScreen extends StatelessWidget {
+  const InquinamentoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -200,8 +227,45 @@ class DettagliScreen extends StatelessWidget {
           children: [
             infoBullet("Dettaglio 1", Icons.check, Colors.green),
             infoBullet("Dettaglio 2", Icons.warning, Colors.red),
-            infoBullet("Dettaglio 2", Icons.warning, Colors.red),
-            infoBullet("Dettaglio 2", Icons.warning, Colors.red),
+            infoBullet("Dettaglio 3", Icons.warning, Colors.red),
+            infoBullet("Dettaglio 4", Icons.warning, Colors.red),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget infoBullet(String text, IconData iconData, Color color) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6.0),
+      child: Row(
+        children: [
+          Icon(iconData, color: color, size: 30),
+          SizedBox(width: 8),
+          Text(text, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,)),
+        ],
+      ),
+    );
+  }
+}
+
+
+class DettagliMeteoScreen extends StatelessWidget {
+  const DettagliMeteoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dettagli"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            infoBullet("Dettaglio Meteo", Icons.check, Colors.green),
+
           ],
         ),
       ),
